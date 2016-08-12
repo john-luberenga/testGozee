f269d972ae67ff2ce931938f3b1f906c9c12a0c8
john-luberenga <john.luberenga@gmail.com>
2016-08-12 17:01:14 +0300

    friday 12th push

--- src/com/gozee/tests/FullGozee2.java
+++ src/com/gozee/tests/FullGozee2.java
@@ -3,6 +3,7 @@ package com.gozee.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FullGozee2 {
@@ -11,6 +12,7 @@ public class FullGozee2 {
 	public static void main(String[] args) throws InterruptedException {
 		//Create firefox driver to drive the browser
 		WebDriver driver = new FirefoxDriver();
+		
 		//System.setProperty("webdriver.chrome.driver","C:\\John BackUp\\PROGRAMMING\\SOFTWARE\\chromedriver_win32\\chromedriver.exe");
 		//WebDriver driver = new ChromeDriver();
 		
@@ -27,47 +29,49 @@ public class FullGozee2 {
 		//Code to read the place holders in each section
 		
 		
-		driver.findElement(By.id("ember808")).sendKeys("Uganda");
-		driver.findElement(By.id("ember815")).click();
+		driver.findElement(By.id("ember843")).sendKeys("Uganda");
+		driver.findElement(By.id("ember855")).click();
 		Thread.sleep(2000);
 		System.out.println("Number of Facilities in Uganda:" + driver.findElement(By.className("result-text")).getText());	   
+		Thread.sleep(2000);
+		
 		
 		//Code to scroll back
 		driver.navigate().back();
-		Thread.sleep(2000);
-		driver.findElement(By.id("ember1011")).sendKeys("Kenya");
-		driver.findElement(By.id("ember1017")).click();
-		Thread.sleep(2000);
+		Thread.sleep(5000);
+		driver.findElement(By.id("ember1055")).sendKeys("Kenya");
+		driver.findElement(By.id("ember1058")).click();
+		Thread.sleep(5000);
 		System.out.println("Number of Facilities in Kenya:" + driver.findElement(By.className("result-text")).getText());
 		
 		//Code to scroll back
 		driver.navigate().back();
 		Thread.sleep(2000);
-		driver.findElement(By.id("ember1091")).sendKeys("Kampala");
-		driver.findElement(By.id("ember1097")).click();
+		driver.findElement(By.id("ember1136")).sendKeys("Kampala");
+		driver.findElement(By.id("ember1139")).click();
 		Thread.sleep(2000);
 		System.out.println("Number of Facilities in Kampala:" + driver.findElement(By.className("result-text")).getText());	
 		
 		jse.executeScript("scroll(0,400)");
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		
 		driver.findElement(By.xpath("//h3[@title='Care Clinic - test']")).click();
 		jse.executeScript("scroll(0,-400)");
 		Thread.sleep(2000);
-		driver.findElement(By.id("ember1517")).click();
+		driver.findElement(By.id("ember1563")).click();
 		
 		jse.executeScript("scroll(0,400)");	
 		
 		//Code to select Calendar
-		driver.findElement(By.className("slot can-create")).click();		
-		driver.findElement(By.id("ember2029")).sendKeys("John");
-		driver.findElement(By.id("ember2031")).sendKeys("Smith");
-		driver.findElement(By.id("ember2054")).sendKeys("Medical Checkup");
-		driver.findElement(By.id("ember2059")).sendKeys("256779127009");
+		//driver.findElement(By.className("slot can-create")).click();		
+		//driver.findElement(By.id("ember2029")).sendKeys("John");
+		//driver.findElement(By.id("ember2031")).sendKeys("Smith");
+		//driver.findElement(By.id("ember2054")).sendKeys("Medical Checkup");
+		//driver.findElement(By.id("ember2059")).sendKeys("256779127009");
 		
 		
 		
-		driver.navigate().to("http://next.cliniccommunicator.com");
+		//driver.navigate().to("http://staging.cliniccommunicator.com");
 
 	}
 
